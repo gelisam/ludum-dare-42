4a6487d148f2b4d44305501ccded293d92b4f8e0
Samuel Gélineau <gelisam+github@gmail.com>
2017-07-29 11:32:34 -0400

    fewer parameters thanks to local scoping

--- main.ts
+++ main.ts
@@ -13,34 +13,6 @@ function Just<A>(value: A): Just<A> {
 }
 
 
-type Images = HTMLElement[][];
-
-function createTable(width: number, height: number) {
-  let table = document.createElement("TABLE");
-  let images: Images = [];
-
-  for (let y = 0; y<height; ++y) {
-    let tr = document.createElement("TR");
-    let image_row: HTMLElement[] = [];
-
-    for (let x = 0; x<width; ++x) {
-      let td = document.createElement("TD");
-      let img = document.createElement("IMG");
-
-      td.appendChild(img);
-      tr.appendChild(td);
-      image_row.push(img);
-    }
-
-    table.appendChild(tr);
-    images.push(image_row)
-  }
-
-  document.body.appendChild(table);
-  return images;
-}
-
-
 type Pos = {"x": number, "y": number};
 
 const dirN = {"x":  0, "y": -1};
@@ -54,39 +26,72 @@ function add(pos1: Pos, pos2: Pos) {
 
 
 
-type Cell = string;
-type Level = string[];
-
-function srcForCell(cell: Cell) {
-  if (cell === "B") return "images/battery.png";
-  if (cell === ".") return "images/floor.png";
-  if (cell === "O") return "images/plugged-outlet.png";
-  if (cell === "R") return "images/plugged-robot.png";
-  if (cell === "S") return "images/solar.png";
-  if (cell === "o") return "images/unplugged-outlet.png";
-  if (cell === "r") return "images/unplugged-robot.png";
-  if (cell === "#") return "images/wall.png";
-  return "";
-}
-
-function loadLevel(width: number, height: number, images: Images, level: Level) {
-  let player: Maybe<Pos> = Nothing;
-
-  for (let y = 0; y<height; ++y) {
-    for (let x = 0; x<width; ++x) {
-      const cell = level[y][x];
-      if (cell === "R" || cell === "r") player = Just({"x": x, "y": y});
-      images[y][x].setAttribute("src", srcForCell(cell));
-    }
-  }
-
-  return player;
-}
-
 window.onload = function() {
   const width = 6;
   const height = 3;
-  let player = loadLevel(width, height, createTable(width, height), [
+
+
+  type Images = HTMLElement[][];
+
+  function createTable() {
+    let table = document.createElement("TABLE");
+    let images: Images = [];
+
+    for (let y = 0; y<height; ++y) {
+      let tr = document.createElement("TR");
+      let image_row: HTMLElement[] = [];
+
+      for (let x = 0; x<width; ++x) {
+        let td = document.createElement("TD");
+        let img = document.createElement("IMG");
+
+        td.appendChild(img);
+        tr.appendChild(td);
+        image_row.push(img);
+      }
+
+      table.appendChild(tr);
+      images.push(image_row)
+    }
+
+    document.body.appendChild(table);
+    return images;
+  }
+
+  const images = createTable();
+
+
+  type Cell = string;
+  type Level = string[];
+
+  function srcForCell(cell: Cell) {
+    if (cell === "B") return "images/battery.png";
+    if (cell === ".") return "images/floor.png";
+    if (cell === "O") return "images/plugged-outlet.png";
+    if (cell === "R") return "images/plugged-robot.png";
+    if (cell === "S") return "images/solar.png";
+    if (cell === "o") return "images/unplugged-outlet.png";
+    if (cell === "r") return "images/unplugged-robot.png";
+    if (cell === "#") return "images/wall.png";
+    return "";
+  }
+
+  function loadLevel(level: Level) {
+    let player: Maybe<Pos> = Nothing;
+
+    for (let y = 0; y<height; ++y) {
+      for (let x = 0; x<width; ++x) {
+        const cell = level[y][x];
+        if (cell === "R" || cell === "r") player = Just({"x": x, "y": y});
+        images[y][x].setAttribute("src", srcForCell(cell));
+      }
+    }
+
+    return player;
+  }
+
+
+  let player = loadLevel([
     "#O##o#",
     ".R....",
     "......",
