f867a5fa00def4b984834449c30d04290ece9148
Samuel Gélineau <gelisam+github@gmail.com>
2017-07-29 17:17:59 -0400

    preserve alpha when scaling up

--- scale-up-images.sh
+++ scale-up-images.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
+
+function pngtopam {
+  # because pngtopnm doesn't output with RGB_ALPHA files.
+  INPUT="$(cat "$@" | base64)"
+  pamstack -tupletype=RGB_ALPHA <(echo "$INPUT" | base64 -D | pngtopnm) \
+                                <(echo "$INPUT" | base64 -D | pngtopnm -alpha)
+}
+
+function pamtopng {
+  # because pnmtopng doesn't accept RGB_ALPHA files.
+  INPUT="$(cat "$@" | base64)"
+  pnmtopng -force <(echo "$INPUT" | base64 -D | pamchannel 0 1 2) \
+           -alpha=<(echo "$INPUT" | base64 -D | pamchannel 3)
+}
+
 for x in `ls small-images/*.png`; do
   F="$(basename "$x")"
-  pngtopnm "small-images/$F" | pamscale 2 | pnmtopng > "images/$F"
+  pngtopam "small-images/$F" | pamscale 2 | pamtopng > "images/$F"
 done
